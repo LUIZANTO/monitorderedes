6f0e7ff6760ec822a376969870ddd879f4df7e63
LUIZANTO <50890833+LUIZANTO@users.noreply.github.com>
2019-06-04 21:58:35 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,46 +1,85 @@
 <html>
-<head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity=
-"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="css/estilo.css">
-<!-- comentario -->
+<head><meta charset="utf-8">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="CSS/estilo.css">
 <title>Monitor de Redes</title>
+<?php require_once "action/db_connect.php"; ?>
 </head>
 <body>
 <div class="container">
 <h1>Monitor de Redes</h1>
-<form method="post" Class="from-inline">
 
+<form method="post" class="form-inline" action="action/insert.php">
 <div class="row">
 
 <div class="col">
 <input type="text" placeholder="Nome" name="nomecomputador" class="form-control">
 </div>
 
-<div Class="col">
+<div class="col">
 <input type="text" placeholder="IP" name="ip" class="form-control">
 </div>
 
 <div class="col">
-<input type="submit" class="btn btn-outline-secondary" value="salvar"/>
+<input type="submit" class="btn btn-outline-secondary btn-personalizado" value="salvar"/>
+</div>
 </div>
 </form>
 
+
+<?php
+function verificarStatus($ip){
+$ping = `ping $ip -n 1 -1 1`;
+if (preg_match("/bytes=/", $ping )) {
+	return true;
+} else {
+	return false;
+{
+}	
+?>
+
 <div class="row">
 <div class="col">
+
+
+<?php
+
+$sql = "select * from computadores";
+
+$result = $connect->query($sql);
+
+	while($dado =  $result->fetch_assoc()) {
+		$nome = $dado['nome'];
+		$ip   = $dado['ip'];
+		$id   = $dado['id'];
+		if(verificarStatus($ip) == true)
+		{
+			echo $nome." - ".$ip. " -online <br>";
+		}
+		else
+		{
+
+			echo $nome." -".$ip. "-offline <br>";
+		}			
+		
+		/*echo $nome . " - " . $ip . "-
+		<a href='action/delete.php?id=".$id."'>Excluir</a>
+		"."<br>";*/
+	}
+?>
+</div>
+</div>
+
 <div class="bloco online">
-<b>127.0.0.1</b><br />
-ONLINE</div>
+<b>127.0.0.1</b><br>
+ONLINE
+</div>
+
 <div class="bloco offline">
-<b>127.0.0.2</b><br />
-OFFLINE</div>
+<b>127.0.0.2</b><br>
+OFFLINE
 </div>
 </div>
-
-
-
-
 
 </body>
 </html>
